401c8aecab21cc5f2d8f539385c05940d226515a
Peterson Adami Candido <me@wetrustinprize.com>
2024-10-08 03:07:27 -0300

    feat: crosshair + hitscan :3

--- gamemode/client/hud/crosshair/cl_init.lua
+++ gamemode/client/hud/crosshair/cl_init.lua
@@ -1,39 +1,53 @@
 include('cl_utils.lua')
 
+local tex_crosshair = Material("smashmod/crosshair.png", "noclamp smooth")
+local tex_hitscan = Material("smashmod/hitscan.png", "noclamp smooth")
+
+local alpha = 0
+local hideSpeed = 0.25
+
 function DrawCrosshair()
     local wep = LocalPlayer():GetActiveWeapon()
-    local crosshairThick, crosshairSize = 2, 30
+    local crosshairSize = 30
+    local hitscanSize = crosshairSize * 1.5
     local h, w = ScrH(), ScrW()
 
+    -- Draw crosshair
     surface.SetDrawColor(255, 255, 255, 255)
+    surface.SetMaterial(tex_crosshair)
 
-    -- Horizontal
-    surface.DrawRect(
-        w / 2 - crosshairThick / 2, 
-        h / 2 - crosshairSize / 2, 
-        2,
+    surface.DrawTexturedRect(
+        w / 2 - crosshairSize / 2,
+        h / 2 - crosshairSize / 2,
+        crosshairSize,
         crosshairSize
     )
 
-    -- Vertical
-    surface.DrawRect(
-        w / 2 - crosshairSize / 2, 
-        h / 2 - crosshairThick / 2, 
-        crosshairSize,
-        2
+    -- Draw hitscan
+    surface.SetDrawColor(255, 255, 255, 255 * alpha)
+
+    surface.SetMaterial(tex_hitscan)
+    surface.DrawTexturedRect(
+        w / 2 - hitscanSize / 2,
+        h / 2 - hitscanSize / 2,
+        hitscanSize,
+        hitscanSize
     )
 
     -- Draw ammo
     if IsValid(wep) then
         DrawCircleWithSquares(
-            w /2, 
-            h / 2, 
-            wep:GetMaxClip1(), 
-            wep:GetMaxClip1() - wep:Clip1() + 1, 
+            w /2,
+            h / 2,
+            wep:GetMaxClip1(),
+            wep:GetMaxClip1() - wep:Clip1() + 1,
             20,
             2
         )
     end
+
+    -- Animations
+    alpha = math.max(0, alpha - FrameTime() / hideSpeed)
 end
 
 -- Add crosshair hook
@@ -45,4 +59,5 @@ net.Receive("smod.damage", function()
     local amount = net.ReadFloat()
 
     LocalPlayer():EmitSound("smashmod/hit.mp3", 75, math.random(85, 115))
+    alpha = 1
 end)
--- gamemode/server/resources.lua
+++ gamemode/server/resources.lua
@@ -4,3 +4,6 @@ resource.AddFile("sound/smashmod/hit.wav");
 resource.AddFile("sound/smashmod/hit/s.mp3");
 resource.AddFile("sound/smashmod/hit/m.mp3");
 resource.AddFile("sound/smashmod/hit/l.mp3");
+
+resource.AddFile("materials/smashmod/crosshair.png");
+resource.AddFile("materials/smashmod/hitscan.png");
